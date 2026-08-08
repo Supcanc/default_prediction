23a3020e5cd77c7e286c12c93b026e48392e3603
Ilya <i.v.nidelko@gmail.com>
2026-08-08 15:13:24 +0500

    Prepared for insertion CSV-file and script added

--- sql_scripts/table_insertion.py
+++ sql_scripts/table_insertion.py
@@ -7,13 +7,16 @@ from db_connect_args import config
 
 db = mysql.connector.connect(
     **config,
-    database='default_prediction'
+    database='default_prediction',
+    allow_local_infile=True
 )
 
 cursor = db.cursor()
 
 cursor.execute("""
-
+LOAD DATA LOCAL INFILE 'dataset/prepared_for_insertion.csv' INTO TABLE credit_risk
+FIELDS TERMINATED BY ','
+IGNORE 1 LINES;
 """)
 
 db.commit()
